60e0e49f8b97db9c2ddbdbe9fcb4f7f056e2ce38
wecantfixthat <41808571+wecantfixthat@users.noreply.github.com>
2021-09-25 00:28:21 -0500

    Fixed some methods. Added getClassGradeAverage, addCourse, deleteCourse, and addStudentToCourse methods.

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.nimbus.State;
 import java.io.*;
 import java.math.BigDecimal;
 import java.sql.*;
@@ -14,18 +15,26 @@ public class JDBC {
             System.out.println("Could not connect to database.\n   Please try again later.");
         }
 
-        System.out.println(addStudent("Berry", "Johnson", "berJo@gmail.com", 4));
-        System.out.println(addStudent("Jimmy", "Neutron", "brainblast@gmail.com", 4));
+        System.out.println(addStudent("Berry", "Johnson", "berJo@gmail.com", 3.8));
+        System.out.println(addStudent("Jimmy", "Neutron", "brainblast@gmail.com", 3.4));
         System.out.println(deleteStudent(1));
-        System.out.println(addStudent("Barney", "The big red dog", "berensteinbears@gmail.com", 4));
+        System.out.println(addStudent("Barney", "The big red dog", "berensteinbears@gmail.com", 3.1));
         System.out.println(addStudent("Fred", "Fazbear", "FredF123@aol.com", 3.5));
         System.out.println(addStudent("Johnny", "Appleseed", "JApple@gmail.com", 3.2));
+        System.out.println("Creating exam: " + createExam(1, "this"));
         System.out.println("Student 1's gpa is: " + getStudentGPA(1));
         System.out.println("Student 5's gpa is: " + getStudentGPA(5));
         System.out.println(addExamScore(2, 1, 90));
         System.out.println(addExamScore(5, 1, 80));
         System.out.println("Student 2's exam score is: " + getStudentExamScore(2, 1));
         System.out.println("Student 5's exam score is: " + getStudentExamScore(5, 1));
+        System.out.println("Adding course: " + addCourse(1,1,"CMSC"));
+        //System.out.println("Deleting course: " + deleteCourse(1));
+        System.out.println("Adding student: " + addStudentToCourse(2, 1, 80.2));
+        System.out.println("Adding student: " + addStudentToCourse(3, 1, 90));
+        System.out.println("Adding student: " + addStudentToCourse(4, 1, 50));
+        System.out.println("The class average for class 1 is: " + getClassGradeAverage(1));
+
     }
 
     public static void connector() throws ClassNotFoundException, SQLException {
@@ -33,7 +42,7 @@ public class JDBC {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306";
         String user = "root";
-        String password = "Marshallaw1";
+        String password = "Chocolate123";
         connection = DriverManager.getConnection(url, user, password);
     }
 
@@ -90,27 +99,48 @@ public class JDBC {
         return result > 0;
     }
 
+    public static boolean createExam(int exam_number, String ... nameDesc) {
+        int result = 0;
+        String name = nameDesc.length > 0 ? nameDesc[0]: null;
+        String desc = nameDesc.length > 1 ? nameDesc[1]: null;
+
+        try{
+            Statement statement = connection.createStatement();
+            String query = String.format("insert into exam(name, description) values ('%s', '%s');", name, desc);
+            result = statement.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("Exam could not be created.");
+            e.printStackTrace();
+        }
+        return result > 0;
+    }
     /**
      * Adds the exam score for the corresponding Student
      *
-     * @param IDNumber the Students ID number
+     * @param IDNumber the Student's ID number
      * @param ExamNumber The Exam number where the score needs to be added
      * @param ExamScore the Exam score that is added
      * @return returns true if score is added, false if score is not added
      */
-    public static boolean addExamScore(int IDNumber, int ExamNumber, int ExamScore){
+    public static boolean addExamScore(int IDNumber, int ExamNumber, double ExamScore){
         int result = 0;
         try {
             Statement statement = connection.createStatement();
-            String query = String.format("insert into student_exam(exam_grade) where student_ID, exam_number, exam_grade values ('%d', '%d', '%d')", IDNumber, ExamNumber, ExamScore);
+            String query = String.format("insert into student_exam(student_ID, exam_number, exam_grade) values (%d, %d,%f);", IDNumber, ExamNumber, ExamScore);
             result = statement.executeUpdate(query);
         } catch (SQLException e){
             System.out.println("There was a problem adding exam score");
+            e.printStackTrace();
         }
         return result > 0;
     }
 
-
+    /**
+     * Deletes a student given the student's ID Number
+     * @param IDNumber student's ID number
+     * @return true if the student was successfully deleted from the database
+     * false if the student could not be deleted from the database
+     */
     public static boolean deleteStudent(int IDNumber) {
         int result = 0;
         try {
@@ -123,22 +153,23 @@ public class JDBC {
         return result > 0;
     }
 
-    public static BigDecimal getStudentGPA(int IDNumber) {
-        BigDecimal gpa = new BigDecimal("0.0");
+    /**
+     * Get a student's current semester GPA by passing in the student's ID number
+     * @param IDNumber the student's ID number
+     * @return the student's current semester GPA
+     */
+    public static double getStudentGPA(int IDNumber) {
+        double gpa = 0;
         try {
             Statement statement = connection.createStatement();
             String query = String.format("select gpa from student where student_ID = %d", IDNumber);
             ResultSet rs = statement.executeQuery(query);
 
             while (rs.next()) {
-                //gpa = rs.getDouble("gpa");
-                //gpa = rs.getFloat("gpa");
-                gpa = rs.getBigDecimal("gpa");
-
+                gpa = rs.getDouble("gpa");
             }
         } catch (SQLException e) {
             System.out.println("There was a problem locating the student's GPA");
-
         }
         return gpa;
     }
@@ -153,7 +184,7 @@ public class JDBC {
         int score = 0;
         try{
             Statement statement = connection.createStatement();
-            String query = String.format("select exam_grade from student_exam where student_ID, exam_number values('%d', '%d')", IDNumber, ExamNumber);
+            String query = String.format("select exam_grade from student_exam where student_ID = %d and exam_number = %d", IDNumber, ExamNumber);
             ResultSet result = statement.executeQuery(query);
             while (result.next()){
                 score = result.getInt("exam_grade");
@@ -165,4 +196,77 @@ public class JDBC {
         return score;
     }
 
+    /**
+     * Add a course to the system
+     * @param courseID the ID of the course
+     * @param professorID the ID of the professor teaching the class
+     * @param courseName the name of the course
+     * @param desc the description of the course
+     * @return
+     */
+    public static boolean addCourse(int courseID, int professorID, String courseName, String ... desc) {
+        int result = 0;
+        String description = desc.length > 0 ? desc[0] : null;
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("insert into professor values(1, 'bob', 'marley', 'bobmarley@gmail.com');");
+            String query = String.format("insert into course(course_ID, professor_ID, course_name, course_description) values(%d, %d, '%s', '%s')", courseID, professorID, courseName, description);
+            result = statement.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("Course could not be added.");
+            e.printStackTrace();
+        }
+        return result > 0;
+    }
+
+    public static boolean addStudentToCourse(int student_ID, int course_ID, double course_grade) {
+        int result = 0;
+        try {
+            Statement statement = connection.createStatement();
+            String query = String.format("insert into course_enrollment values(%d, %d, %f)", student_ID, course_ID, course_grade);
+            result = statement.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("The student could not be added to the course.");
+            e.printStackTrace();
+        }
+        return result > 0;
+    }
+
+    /**
+     * Delete the course with the associated course ID number
+     * @param courseID the course to be deleted
+     * @return true if the course was deleted
+     * false if the course could not be deleted
+     */
+    public static boolean deleteCourse(int courseID) {
+        int result = 0;
+        try {
+            Statement statement = connection.createStatement();
+            String query = String.format("delete from course where course_ID = %d;", courseID);
+            result = statement.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println("The course with Course ID: '" + courseID + "' could not be deleted.");
+        }
+        return result > 0;
+    }
+
+    /**
+     * Get the average percentage of all the students in the class
+     * @param courseID the class that the average is calculated from
+     * @return the class grade average
+     */
+    public static double getClassGradeAverage(int courseID) {
+        double avg = 0;
+        try {
+            Statement statement = connection.createStatement();
+            String query = String.format("select avg(course_grade) from course_enrollment where course_ID = %d;", courseID);
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()) {
+                avg = result.getDouble("avg(course_grade)");
+            }
+        } catch (Exception e) {
+            System.out.println("The class grade average could not be calculated.");
+        }
+        return avg;
+    }
 }
